be87c43c36d4c5ae5847a61ccfbfc71b7b855a88
wuttipat <a_wuttipat@hotmail.com>
2014-11-25 23:38:59 +0700

    update

--- fontPage/symptom.php
+++ fontPage/symptom.php
@@ -1,8 +1,21 @@
 <?php
 	require_once '../database/database.php';
+	$count = 0;
 	$id = $_GET['id'];
-	$data = db_select('symptom', null ,'WHERE id_symptom ='. $id);
+	$data = db_select('symptom', null ,'WHERE type_wrong ='. $id);
 ?>
 <table>
-	
+	<tr>
+	<?if(count($data) > 0):?>
+		<?foreach($data as $key => $value):?>
+			<td><?= $value['symptom']?></td>
+			<?php 
+				$count++;
+				if($count %3 == 0){
+					echo '</tr><tr>';
+				}
+			?>
+		<?endforeach ?>
+	<?endif?>
+	</tr>
 </table>
